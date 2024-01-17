91262707f8cb100d7ff13242fc43de612d348ca6
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-01-18 00:16:17 +0530

    Create Database Tables form Java Code

--- 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -20,7 +20,7 @@ public class CruddemoApplication {
 
 			// createStudent(studentDAO);
 
-			// createMultipleStudents(studentDAO);
+			 createMultipleStudents(studentDAO);
 
 			// readStudent(studentDAO);
 
@@ -32,7 +32,9 @@ public class CruddemoApplication {
 
 			// deleteStudent(studentDAO);
 
-			deleteAllStudent(studentDAO);
+			// deleteAllStudent(studentDAO);
+
+
 		};
 	}
 
