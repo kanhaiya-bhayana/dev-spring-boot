c30e090955b828252c488fe3d74de07d953d48b3
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-01-21 14:34:49 +0530

    DB - CRUD - REST - All CRUD operations has been implemented

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/octocat/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/octocat/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.octocat.springboot.cruddemo.service;
 
 import com.octocat.springboot.cruddemo.dao.IEmployeeDAO;
 import com.octocat.springboot.cruddemo.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +20,22 @@ public class EmployeeServiceImpl implements IEmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+       return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/octocat/springboot/cruddemo/service/IEmployeeService.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/octocat/springboot/cruddemo/service/IEmployeeService.java
@@ -7,5 +7,8 @@ import java.util.List;
 public interface IEmployeeService {
 
     List<Employee> findAll();
+    Employee findById(int theId);
+    Employee save(Employee theEmployee);
+    void deleteById(int theId);
 
 }
