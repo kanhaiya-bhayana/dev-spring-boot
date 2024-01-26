acc414cb4b76b60501969bd854c4bc4c61473165
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-01-26 23:02:23 +0530

    HTML thymeleaf FORM with model added

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octocat/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octocat/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,12 @@
 package com.octocat.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -13,8 +18,44 @@ public class HelloWorldController {
     }
 
     // need a controller method to process the HTML form
-    @GetMapping("/processForm")
+    @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+
+    // need a controller method to read form data and
+    // add data to model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create a message
+        String result = "Yo! " + theName;
+
+        // add message to model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create a message
+        String result = "Hey " + theName;
+
+        // add message to model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
