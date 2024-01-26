a50d66ca5793171c380819446a60d344b1ae201b
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-01-26 23:22:51 +0530

    Dropdown implementation for dynamic data added

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octocat/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octocat/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,21 @@
 package com.octocat.springboot.thymeleafdemo.controller;
 
 import com.octocat.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
@@ -19,6 +26,9 @@ public class StudentController {
         // add student object to the model
         theModel.addAttribute("student", theStudent);
 
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
+
         return "student-form";
     }
 
@@ -26,7 +36,7 @@ public class StudentController {
     public String processForm(@ModelAttribute("student") Student theStudent){
 
         // log the input data
-        System.out.println("theStudent: "+ theStudent.getFirstName() + " " + theStudent.getLastName());
+        System.out.println("theStudent: "+ theStudent.toString());
         return "student-confirmation";
     }
 }
--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octocat/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octocat/springboot/thymeleafdemo/model/Student.java
@@ -3,6 +3,7 @@ package com.octocat.springboot.thymeleafdemo.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
     public Student(){
 
@@ -24,11 +25,20 @@ public class Student {
         this.lastName = lastName;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
                 '}';
     }
 }
