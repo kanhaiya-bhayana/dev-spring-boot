be77bfaa6a300e84de97c10d2055f2edc950045c
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-01-30 13:04:37 +0530

    Update employee functionality added

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-thymeleafApplication/src/main/java/com/octocat/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-thymeleafApplication/src/main/java/com/octocat/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,8 +5,7 @@ import com.octocat.springboot.thymeleafdemo.service.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,6 +41,29 @@ public class EmployeeController {
 
         theModel.addAttribute("employee", theEmployee);
 
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee request){
+
+        // save the employee
+        employeeService.save(request);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+
+        // get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee as a model attribute to pre-populate the form
+        theModel.addAttribute("employee",theEmployee);
+
+        // send over to our form
         return "employees/employee-form";
     }
 }
