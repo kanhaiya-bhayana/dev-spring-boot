109b5f154761ea8a13c301d8895cffb27e00c903
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-02-04 21:12:01 +0530

    one-to-one bi-directional mapping completed

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-hb_01_one_to_one_bi/src/main/java/com/octocat/cruddemo/dao/AppDAO.java
@@ -0,0 +1,13 @@
+package com.octocat.cruddemo.dao;
+
+import com.octocat.cruddemo.entity.Instructor;
+import com.octocat.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+    Instructor findInstructorById(int theId);
+    void deleteInstructorById(int theId);
+    InstructorDetail findInstructorDetailById(int theId);
+    void deleteInstructorDetailById(int theId);
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-hb_01_one_to_one_bi/src/main/java/com/octocat/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,73 @@
+package com.octocat.cruddemo.dao;
+
+import com.octocat.cruddemo.entity.Instructor;
+import com.octocat.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    // define field for entity manager
+
+    // inject entity manger using constructor injection
+    private EntityManager entityManager;
+    @Autowired
+    public AppDAOImpl(EntityManager theEntityManger){
+        this.entityManager = theEntityManger;
+    }
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class,theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+
+        // retrieve the instructor
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+
+        if (tempInstructor == null) {
+            System.out.println("Instructor not exist");
+            return;
+        }
+
+        // delete the instructor
+        entityManager.remove(tempInstructor);
+        System.out.println("Deleted");
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class,theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+        // retrieve the instructor
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
+
+        if (tempInstructorDetail == null) {
+            System.out.println("InstructorDetail not exist");
+            return;
+        }
+
+        // remove the associated reference
+        // break bi-directional link
+        //
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+        // delete the instructorDetail
+        entityManager.remove(tempInstructorDetail);
+        System.out.println("Deleted");
+    }
+}
