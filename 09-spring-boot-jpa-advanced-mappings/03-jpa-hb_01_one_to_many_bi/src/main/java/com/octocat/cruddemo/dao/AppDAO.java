d6a1cd62a1fbb1a67e834b033673daae555d8dfd
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-02-05 23:17:52 +0530

    Eager and Lazy Loading initials

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-hb_01_one_to_many_bi/src/main/java/com/octocat/cruddemo/dao/AppDAO.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-hb_01_one_to_many_bi/src/main/java/com/octocat/cruddemo/dao/AppDAO.java
@@ -1,7 +1,10 @@
 package com.octocat.cruddemo.dao;
 
+import com.octocat.cruddemo.entity.Course;
 import com.octocat.cruddemo.entity.Instructor;
 import com.octocat.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
 
 public interface AppDAO {
 
@@ -10,4 +13,5 @@ public interface AppDAO {
     void deleteInstructorById(int theId);
     InstructorDetail findInstructorDetailById(int theId);
     void deleteInstructorDetailById(int theId);
+    List<Course> findCoursesByInstructorId(int theId);
 }
--- 09-spring-boot-jpa-advanced-mappings/03-jpa-hb_01_one_to_many_bi/src/main/java/com/octocat/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-hb_01_one_to_many_bi/src/main/java/com/octocat/cruddemo/dao/AppDAOImpl.java
@@ -1,11 +1,15 @@
 package com.octocat.cruddemo.dao;
 
+import com.octocat.cruddemo.entity.Course;
 import com.octocat.cruddemo.entity.Instructor;
 import com.octocat.cruddemo.entity.InstructorDetail;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class AppDAOImpl implements AppDAO{
@@ -70,4 +74,21 @@ public class AppDAOImpl implements AppDAO{
         entityManager.remove(tempInstructorDetail);
         System.out.println("Deleted");
     }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int theId) {
+
+        // create query
+        TypedQuery<Course> query = entityManager.createQuery(
+                "from Course where instructor.id = :data",Course.class
+        );
+        query.setParameter("data", theId);
+
+        // execute the query
+        List<Course> courses = query.getResultList();
+
+        return courses;
+    }
+
+
 }
