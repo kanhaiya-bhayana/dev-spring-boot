06dff14265c179992c697e0954de072da20ad717
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-02-16 00:09:24 +0530

    AOP - @Before advice 'only-match'

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/octocat/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/octocat/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,7 @@ public class MyDemoLoggingAspect {
 
     // let's start with a @Before advice
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.octocat.aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccount(){
 
         System.out.println("\n =========> Executing @Before advice on addAccount()");
