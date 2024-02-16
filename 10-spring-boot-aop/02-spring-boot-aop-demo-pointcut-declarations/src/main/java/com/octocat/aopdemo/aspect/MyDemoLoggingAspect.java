62ecba861650e3749590ef4bc67891811e3f0e7a
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-02-17 00:24:29 +0530

    get method signature with 'JoinPoints'

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/octocat/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/octocat/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,10 @@
 package com.octocat.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,15 @@ import org.springframework.stereotype.Component;
 @Order(2)
 public class MyDemoLoggingAspect {
     @Before("com.octocat.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccount(){
+    public void beforeAddAccount(JoinPoint theJoinPoint){
+
+        // JoinPoint has metadata about method call
         System.out.println(" =========> Executing @Before advice on addAccount()");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
+
+        // display the method arguments
     }
 }
