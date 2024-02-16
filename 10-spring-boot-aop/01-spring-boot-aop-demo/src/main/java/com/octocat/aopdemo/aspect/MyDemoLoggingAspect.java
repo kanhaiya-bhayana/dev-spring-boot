8f772b6c494f4c3a2801effbeb51172f9e96bc2b
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-02-16 22:49:53 +0530

    Match any parameters using wildcard '..'

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/octocat/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/octocat/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -13,7 +13,7 @@ public class MyDemoLoggingAspect {
     // let's start with a @Before advice
 
     // @Before("execution(public void add*())")
-    @Before("execution(* add*(com.octocat.aopdemo.Account, ..))") // remember always give Fully-qualified class name in the parameter
+    @Before("execution(* add*(..))") // remember always give Fully-qualified class name in the parameter
     public void beforeAddAccount(){
 
         System.out.println("\n =========> Executing @Before advice on addAccount()");
